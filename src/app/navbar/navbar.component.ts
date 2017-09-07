788e8d43e230ab23996a12981c42cf3714fcb971
Karine Pham <kpham@k-ph.fr>
2017-09-07 01:55:19 +0100

    Ajout feature login/logout du menu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth }from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'nfs-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  user : firebase.User;
 
-  constructor() { }
+  constructor(private angularFireAuth: AngularFireAuth) {
+    angularFireAuth.authState.subscribe(user => this.user = user);
+   }
 
-  ngOnInit() {
-  }
-
+  logout() {
+    this.angularFireAuth.auth.signOut();
+}
 }
